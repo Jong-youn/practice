e50008e9e129949c86f4306b3634d0c4c5c86af1
ljy6816@gmail.com <ljy6816@gmail.com>
2022-04-20 22:40:02 +0900

    equals

--- src/com/practice/self/Equals.java
+++ src/com/practice/self/Equals.java
@@ -8,18 +8,18 @@ public class Equals {
         String d = new String("aaa");
         String e = c;
 
-        System.out.println("System.identityHashCode(a) = " + System.identityHashCode(a));
+        System.out.println("System.identityHashCode(a) = " + System.identityHashCode(a)); // a = b
         System.out.println("System.identityHashCode(b) = " + System.identityHashCode(b));
-        System.out.println("System.identityHashCode(c) = " + System.identityHashCode(c));
+        System.out.println("System.identityHashCode(c) = " + System.identityHashCode(c)); // c =/ d
         System.out.println("System.identityHashCode(d) = " + System.identityHashCode(d));
 
-        System.out.println(a.equals(b));
-        System.out.println(a.equals(c));
-        System.out.println(a == b);
-        System.out.println(a == c);
+        System.out.println(a.equals(b)); // true
+        System.out.println(a.equals(c)); // true
+        System.out.println(a == b); // true
+        System.out.println(a == c); // false
 
-        System.out.println(c.equals(d));
-        System.out.println(c == d);
+        System.out.println(c.equals(d)); // true
+        System.out.println(c == d); // false
 
         System.out.println(c.equals(e)); //true
         System.out.println(c == e); // true
